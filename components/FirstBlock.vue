6186a55c0e806d717c63b60c5f7d2736a729f054
Djoncarton1921 <shapranov.nik@gmail.com>
2022-11-28 21:52:11 +0200

    the footer was laid out, the component of the first block was added

---
+++ components/FirstBlock.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="first__block">
+      <Header />
+   </div>
+</template>
+ 
+<style lang="scss" scoped>
+.first__block {
+   position: absolute;
+   width: 1440px;
+   height: 940px;
+   left: 0px;
+   top: 0px;
+   background: radial-gradient(68.91% 140.71% at 51.6% 34.95%, #43A99A 0%, #148783 37.59%, #0B5147 100%)
+   ;
+   border-radius: 0px 0px 36px 36px;
+   width: 100%;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+   <FirstBlock />
+   <div class="container">
+   </div>
+</template>
+
+<style lang="scss" scoped>
+   .container {
+      max-width: &maxWidthContainer;
+      margin: 0 auto;
+   }
+</style>
